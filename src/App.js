6b861ca2344caf01b0228f34117de7b1acd1aa8d
Scott Dixon <scott@howrefreshing.com.au>
2017-11-24 15:49:05 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import stackOverflowData from './data';
+
+import toDos from './data';
+import { Container, Row, Col, ListGroup, ListGroupItem, Badge } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to {stackOverflowData.title}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul>
+        {toDos.map(item => (
+          <li>{ item }</li>
+        ))}
+      </ul>
     );
   }
 }
--- src/data.js
+++ src/data.js
@@ -1,53 +1,7 @@
-let stackOverflowData = {
-  title: 'Stack Overflow',
-  navbar_links: ['Questions', 'Developer Jobs', 'Tags', 'Users'],
-  top_questions: [
-    {
-      title: 'What is an alternative to JS?',
-      tags: ['javascript','alternative'],
-      votes: 0,
-      answers: 0,
-      views: 1,
-      asked: '6 minutes ago',
-      author: 'Jane Smith'
-    },
-    {
-      title: 'How do I become a JS ninja?',
-      tags: ['javascript','ninja'],
-      votes: 0,
-      answers: 0,
-      views: 3,
-      asked: '7 minutes ago',
-      author: 'John Smith'
-    },
-    {
-      title: 'Help with KanyeJS',
-      tags: ['javascript','kanye','syntax'],
-      votes: 0,
-      answers: 0,
-      views: 5,
-      asked: '8 minutes ago',
-      author: 'Kim West'
-    },
-    {
-      title: 'SimonJS - my door wont open',
-      tags: ['javascript','simonjs','door','coffee'],
-      votes: 0,
-      answers: 2,
-      views: 20,
-      asked: '10 minutes ago',
-      author: 'Jane Smith'
-    }
-  ],
-  featured_on_meta: ['Best places for dog photos', 'Chocolate shake tips'],
-  hot_meta_posts: [{
-    post: 'I literally just died when I ran out of coffee tho',
-    votes: 20
-  },
-  {
-    post: 'hashtagz4dayz',
-    votes: 13
-  }]
-}
+let toDos = [
+  'Wash Clothes',
+  'Finish Study',
+  'Feed Dog'
+]
 
-export default stackOverflowData;
+export default toDos;
